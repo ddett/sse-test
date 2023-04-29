dfcef16b99451a650770e525449d2ceebf59f1e1
Marc A. Donis <marc@balloonary.io>
2023-04-29 11:01:48 +0200

    render.com seems to insist on launching app.js instead of server.js...... for some reason

---
+++ api/app.js
@@ -0,0 +1,61 @@
+// create an express server
+const express = require('express');
+const server = express();
+
+module.exports = {
+	path: '/api',
+	handler: server,
+};
+
+// // create a route to handle SSE
+// server.get('/sse', (req, res) => {
+//     // set headers for SSE
+//     res.writeHead(200, {
+//         'Content-Type': 'text/event-stream',
+//         'Cache-Control': 'no-cache',
+//         Connection: 'keep-alive',
+//     });
+
+//     // send SSE every second
+//     const interval = setInterval(() => {
+//         res.write(`data: ${new Date().toISOString()}\n\n`);
+//     }, 1000);
+
+//     // stop sending SSE on timeout
+//     req.on('timeout', () => {
+//         clearInterval(interval);
+//         res.end();
+//     });
+// });
+
+server.get('/sse', (req, res) => {
+	console.log('enter sse');
+	console.log('res', res);
+
+	res.setHeader('Cache-Control', 'no-cache');
+	res.setHeader('Content-Type', 'text/event-stream');
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Connection', 'keep-alive');
+	res.flushHeaders(); // flush the headers to establish SSE with client
+
+	let counter = 0;
+	const intervalId = setInterval(() => {
+		counter++;
+		console.log('will res.write. counter', counter);
+		res.write(`data: ${JSON.stringify({ text: 'hi there', num: counter })}\n\n`);
+		if (counter >= 10) {
+			// this is also what OpenAI uses to signal end of stream https://platform.openai.com/docs/api-reference/completions/create#completions/create-stream
+			res.write('data: [DONE]\n\n');
+			clearInterval(intervalId);
+			// res.end(); // terminates SSE session
+			return;
+		}
+	}, 800);
+
+	// If client closes connection, stop sending events
+	res.on('close', () => {
+		console.log('client dropped me');
+		clearInterval(intervalId);
+		res.end();
+	});
+});
