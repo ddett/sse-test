7008e6e0dccde75fc492fdf15c56b9a4784d73fa
Marc A. Donis <marc@balloonary.io>
2023-04-29 11:12:41 +0200

    .

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,67 +1,63 @@
-import path from "path";
-import fs from "fs";
+import path from 'path';
+import fs from 'fs';
 const server = {
-  port: process.env.PORT || 3000,
-  host: process.env.HOST || "0.0.0.0",
+	port: process.env.PORT || 3000,
+	host: process.env.HOST || '0.0.0.0',
 };
 // console.log('process.env.ENV ', process.env.ENV);
-if (
-  process.env.ENV === "dev" &&
-  process.env.CERT_DIR &&
-  process.env.CERT_FILENAME
-) {
-  // for windows, do this:
-  // https://ricktowns.com/post/2020/04/02/windows-10-self-signed-certificate
-  // and then:
-  // https://helpcenter.gsx.com/hc/en-us/articles/115015887447-Extracting-Certificate-crt-and-PrivateKey-key-from-a-Certificate-pfx-File
-  server.https = {
-    key: fs.readFileSync(
-      path.resolve(process.env.CERT_DIR, `${process.env.CERT_FILENAME}.key`)
-    ),
-    cert: fs.readFileSync(
-      path.resolve(process.env.CERT_DIR, `${process.env.CERT_FILENAME}.crt`)
-    ),
-  };
+if (process.env.ENV === 'dev' && process.env.CERT_DIR && process.env.CERT_FILENAME) {
+	// for windows, do this:
+	// https://ricktowns.com/post/2020/04/02/windows-10-self-signed-certificate
+	// and then:
+	// https://helpcenter.gsx.com/hc/en-us/articles/115015887447-Extracting-Certificate-crt-and-PrivateKey-key-from-a-Certificate-pfx-File
+	server.https = {
+		key: fs.readFileSync(
+			path.resolve(process.env.CERT_DIR, `${process.env.CERT_FILENAME}.key`)
+		),
+		cert: fs.readFileSync(
+			path.resolve(process.env.CERT_DIR, `${process.env.CERT_FILENAME}.crt`)
+		),
+	};
 }
 
 export default {
-  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-  ssr: false,
+	// Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
+	ssr: false,
 
-  // Global page headers: https://go.nuxtjs.dev/config-head
-  head: {
-    title: "sse-test",
-    htmlAttrs: {
-      lang: "en",
-    },
-    meta: [
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "" },
-      { name: "format-detection", content: "telephone=no" },
-    ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
-  },
+	// Global page headers: https://go.nuxtjs.dev/config-head
+	head: {
+		title: 'sse-test',
+		htmlAttrs: {
+			lang: 'en',
+		},
+		meta: [
+			{ charset: 'utf-8' },
+			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+			{ hid: 'description', name: 'description', content: '' },
+			{ name: 'format-detection', content: 'telephone=no' },
+		],
+		link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+	},
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+	// Global CSS: https://go.nuxtjs.dev/config-css
+	css: [],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+	// Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+	plugins: [],
 
-  server,
+	server,
 
-  serverMiddleware: ["~/api/server.js"],
+	serverMiddleware: ['~/api/app.js'],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
+	// Auto import components: https://go.nuxtjs.dev/config-components
+	components: true,
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [],
+	// Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+	buildModules: [],
 
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [],
+	// Modules: https://go.nuxtjs.dev/config-modules
+	modules: [],
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+	// Build Configuration: https://go.nuxtjs.dev/config-build
+	build: {},
 };
