50a65545c67cf9a9ceb22d1e6832ef94c14919f1
Marc A. Donis <marc@balloonary.io>
2023-04-29 09:29:37 +0200

    .

--- components/Tutorial.vue
+++ components/Tutorial.vue
@@ -10,6 +10,10 @@ export default {
     console.log('Component mounted.')
 
     this.evtSource = new EventSource('/api/sse');
+    this.evtSource.onerror = err => {
+      console.error('EventSource error', err);
+    }
+
     this.evtSource.onmessage = e => {
       if (e.data === '[DONE]') {
         // this is also what OpenAI uses to signal end of stream https://platform.openai.com/docs/api-reference/completions/create#completions/create-stream
