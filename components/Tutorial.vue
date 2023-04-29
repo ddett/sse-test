bee80e9e7813047412da06fef2bf764f98ba52e9
Dylan Thompson <thompson@kamerakopf.de>
2023-04-29 08:51:00 +0200

    init

---
+++ components/Tutorial.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>SSE Test</div>
+</template>
+
+<script lang="js">
+export default {
+  name: 'Tutorial',
+
+  mounted() {
+    console.log('Component mounted.')
+
+    this.evtSource = new EventSource('/api/sse');
+    this.evtSource.onmessage = e => {
+      if (e.data === '[DONE]') {
+        // this is also what OpenAI uses to signal end of stream https://platform.openai.com/docs/api-reference/completions/create#completions/create-stream
+        console.log('[DONE]', e);
+        this.evtSource.close();
+      } else {
+        console.log('message', e);
+      }
+    };
+  }
+}
+</script>
---
+++ nuxt.config.js
@@ -0,0 +1,69 @@
+import path from "path";
+import fs from "fs";
+const server = {
+  port: process.env.PORT || 3000,
+  host: process.env.HOST || "0.0.0.0",
+};
+// console.log('process.env.ENV ', process.env.ENV);
+if (process.env.ENV === "dev") {
+  // for windows, do this:
+  // https://ricktowns.com/post/2020/04/02/windows-10-self-signed-certificate
+  // and then:
+  // https://helpcenter.gsx.com/hc/en-us/articles/115015887447-Extracting-Certificate-crt-and-PrivateKey-key-from-a-Certificate-pfx-File
+  server.https = {
+    key: fs.readFileSync(
+      path.resolve(
+        "/Users/dthompson/Documents/Projekte/1905_Balloonary/Dev/cert/localhost",
+        "localhost.key"
+      )
+    ),
+    cert: fs.readFileSync(
+      path.resolve(
+        "/Users/dthompson/Documents/Projekte/1905_Balloonary/Dev/cert/localhost",
+        "localhost.crt"
+      )
+    ),
+  };
+}
+
+export default {
+  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
+  ssr: false,
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: "sse-test",
+    htmlAttrs: {
+      lang: "en",
+    },
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
+      { name: "format-detection", content: "telephone=no" },
+    ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [],
+
+  server,
+
+  serverMiddleware: ["~/api/server.js"],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [],
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {},
+};
